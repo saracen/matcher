e41091c9f028c4602441677f7558b8942051974c
Arran Walker <arran.walker@fiveturns.org>
2020-09-06 13:55:07 +0100

    Simplify/avoid stutter in function names

--- matcher_multi.go
+++ matcher_multi.go
@@ -2,12 +2,13 @@ package matcher
 
 type multiMatcher []Matcher
 
-// NewMultiMatcher returns a new Matcher that matches against many matchers.
-func NewMultiMatcher(matches ...Matcher) Matcher {
+// Multi returns a new Matcher that matches against many matchers.
+func Multi(matches ...Matcher) Matcher {
 	return multiMatcher(matches)
 }
 
-// Match uses both the include and exclude patterns to determine matches.
+// Match performs a match with all matchers provided and returns a result
+// early if one matched.
 func (p multiMatcher) Match(pathname string) (Result, error) {
 	var follow bool
 
